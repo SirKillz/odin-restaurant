18412cd5468d517697e4e8abcbc7952fc3fd6e2a
Nick Killeen <nick@aleaquo.com>
2025-03-25 09:06:51 -0500

    Changed name to home.js and home.css to reflect the home page

---
+++ src/home.js
@@ -0,0 +1,21 @@
+import pizzaImage from "./pizza.jpg";
+import "./home.css"
+
+function displayHome() {
+    const contentDiv = document.querySelector("#content");
+    contentDiv.classList.add("content")
+
+    const header = document.createElement("h1");
+    header.textContent = "This is the best pizza in town!"
+
+    const image = document.createElement("img");
+    image.src = pizzaImage;
+    image.alt = "Image of pizza"
+    image.classList.add("pizza-Image")
+
+    contentDiv.appendChild(header);
+    contentDiv.appendChild(image);
+    
+}
+
+export {displayHome}
