893ec78f2f30a15c41e97ae27527c7db34405585
Nick Killeen <nick@aleaquo.com>
2025-03-26 07:35:10 -0500

    Added the event listeners to the nav buttons so that they can be clicked and switch pages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import { displayHome } from "./home";
 import { applyNavStyle } from "./navStyle";
+import { pageSwitch } from "./navLogic";
 
 
 
 document.addEventListener("DOMContentLoaded", (event) => {
     displayHome();
     applyNavStyle();
+})
+
+const navButtons = document.querySelectorAll("button");
+navButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        pageSwitch(event);
+    })
 })
